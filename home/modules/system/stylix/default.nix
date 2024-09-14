cd2f06ef91bb94a3cc42907a169c84d186c330a4
installer <installer@gmail.com>
2024-09-14 15:56:35 +0200

    Fixed the terminal

--- home/modules/system/stylix/default.nix
+++ home/modules/system/stylix/default.nix
@@ -1,8 +1,29 @@
 { config, lib, pkgs, ... }:
-
+let
+  themePath = "./../../../themes/ayu-dark"
+in
 {
   stylix = {
     enable = true;
-    image = ./../../../themes/ayu-dark/background.png;
+    image = themePath + "/background.png";
+    stylix.base16Scheme = themePath + "/colors.yaml";
+
+    fonts = {
+      monospace = {
+        name = "ProggyClean CE Nerd Font";
+      };
+      serif = {
+        name = "ProggyClean CE Nerd Font";
+      };
+      sansSerif = {
+        name = "ProggyClean CE Nerd Font";
+      };
+      sizes = {
+        terminal = 18;
+        applications = 12;
+        popups = 12;
+        desktop = 12;
+      };
+    };
   };
 }
