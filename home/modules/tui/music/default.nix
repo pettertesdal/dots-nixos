27ed7bdd2e95649b50f89d8e604653032e24ee20
installer <installer@gmail.com>
2024-09-23 08:36:38 +0200

    test

--- home/modules/tui/music/default.nix
+++ home/modules/tui/music/default.nix
@@ -1,2 +1,16 @@
+{ config, ... }:
 {
+  services.mpd = {
+    enable = true;
+    musicDirectory = "~/media/music";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "My PipeWire Output"
+      }
+    '';
+  };
+  programs.ncmpcpp = {
+    enable = true;
+  };
 }
