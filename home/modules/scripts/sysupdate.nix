1f51c7a90da3d031befa85eb03a5c3ebd991fa96
installer <installer@gmail.com>
2024-09-26 10:49:48 +0200

    177 current 2024-09-26

--- home/modules/scripts/sysupdate.nix
+++ home/modules/scripts/sysupdate.nix
@@ -4,46 +4,35 @@
     specialisation = pkgs.writeShellScriptBin "sysupdate" ''
       set -e
 
-      # Edit your config
-      $EDITOR configuration.nix
-
       # cd to your config dir
-      pushd ~/dotfiles/nixos/
+      pushd ~/.dots/
 
-      # Early return if no changes were detected (thanks @singiamtel!)
-      if git diff --quiet '*.nix'; then
-        echo "No changes detected, exiting."
-        popd
-        exit 0
+      # Early return if no changes were detected
+      if git ls-files '*.nix' | xargs git diff --quiet; then
+      echo "No changes detected, exiting."
+      popd
+      exit 0
       fi
-
-      # Autoformat your nix files
-      alejandra . &>/dev/null \
-      || ( alejandra . ; echo "formatting failed!" && exit 1)
 
       # Shows your changes
       git diff -U0 '*.nix'
 
       echo "NixOS Rebuilding..."
 
-      # Rebuild, output simplified errors, log trackebacks
-      sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+      # Rebuild, output simplified errors, log tracebacks
+      sudo nixos-rebuild switch >nixos-switch.log 2>&1 || (cat nixos-switch.log | grep --color error && exit 1)
 
       # Get current generation metadata
-      current=$(nixos-rebuild list-generations | grep current)
+      current=$(nixos-rebuild list-generations | grep current | awk '{print $1, $2, $3}')
 
-      # Commit all changes witih the generation metadata
+      # Commit all changes with the generation metadata
       git commit -am "$current"
 
       # Back to where you were
       popd
 
       # Notify all OK!
-      notify-send -e "NixOS Rebuilt OK!" --icon=software-update-available
-      in [
-        specialisation
-      ];
-      }
+      /usr/bin/notify-send "NixOS Rebuilt OK!" --icon=software-update-available
 
     '';
   in [
