89c67bcf923dbbc3f2a601e8c9cd0d0532171da0
installer <installer@gmail.com>
2024-09-26 13:43:55 +0200

    sysupdate

--- home/modules/system/notification/default.nix
+++ home/modules/system/notification/default.nix
@@ -1,10 +1,10 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   inherit (config.lib.stylix.colors) base00 base01 base02 base03 base04 base05 base06 base08;
 in
 {
   services.mako = {
     enable = true;
-    borderColor = "#${base05}";
+    borderColor = lib.mkForce "#${base05}";
   };
 }
