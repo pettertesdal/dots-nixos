52d77bd0487adc7874b1915a477f1cb73f75ee51
installer <installer@gmail.com>
2024-09-26 12:58:33 +0200

    sysupdate

--- home/modules/scripts/sysupdate.nix
+++ home/modules/scripts/sysupdate.nix
@@ -5,18 +5,24 @@
       set -e
 
       # cd to your config dir
-      pushd ~/.dots/
+      pushd ~/.dots/ || { notify-send "Failed to enter config directory!" --icon=dialog-error; exit 1; }
 
       echo "NixOS Rebuilding..."
 
-      sudo nixos-rebuild switch --flake $HOME/.dots/ >nixos-switch.log 2>&1 || (cat nixos-switch.log | grep --color error && exit 1)
+      # Try to rebuild, and handle errors
+      if ! sudo nixos-rebuild switch --flake "$HOME/.dots/" >nixos-switch.log 2>&1; then
+          cat nixos-switch.log | grep --color error
+          notify-send "NixOS Rebuild Failed! 󰅚 " --icon=dialog-error
+          exit 1
+      fi
 
+      # If rebuild is successful, proceed
       git add -A
-      git commit -m "sysupdate"
-      git push
+      git commit -m "sysupdate" || { notify-send "Git Commit Failed!" --icon=dialog-error; exit 1; }
+      git push || { notify-send "Git Push Failed!" --icon=dialog-error; exit 1; }
 
       # Back to where you were
-      popd
+      popd || { notify-send "Failed to return to previous directory!" --icon=dialog-error; exit 1; }
 
       # Notify all OK!
       notify-send "NixOS Rebuilt OK! 󰅠 " --icon=software-update-available
