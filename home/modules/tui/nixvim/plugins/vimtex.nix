6884e6db3d801324a3a73ab590386a75741ea6be
installer <installer@gmail.com>
2024-09-27 21:18:46 +0200

    sysupdate

--- home/modules/tui/nixvim/plugins/vimtex.nix
+++ home/modules/tui/nixvim/plugins/vimtex.nix
@@ -1,7 +1,9 @@
+{ pkgs, ... }:
 {
   programs.nixvim = {
     plugins.vimtex = {
       enable = true;
+      texlivePackage = pkgs.texlive.combined.scheme-full;
 
       settings = {
         view_method = "zathura";
