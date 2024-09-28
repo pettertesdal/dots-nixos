d0a6031b5753fe262de89cc405e8bbbc3ec12a50
installer <installer@gmail.com>
2024-09-28 21:55:08 +0200

    sysupdate

--- home/modules/scripts/notetaking/default.nix
+++ home/modules/scripts/notetaking/default.nix
@@ -3,5 +3,6 @@
   imports = [
     ./rapport.nix
     ./current_lecture.nix
+    ./newlecture.nix
   ];
 }
---
+++ home/modules/scripts/notetaking/newlecture.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+{
+  home.packages = let
+    specialisation = pkgs.writeShellScriptBin "newlecture" ''
+      cd ~/documents/university/
+      CREATION_TYPE=$(echo -e "lecture\ncourse\nproject" | rofi -dmenu -no-custom -p "Select option")
+
+      # Creating a new lecture
+      if [ "$CREATION_TYPE" == "lecture" ]; then
+        cd current_course
+        highest_num=$(ls lec_*.tex 2>/dev/null | grep -oP 'lec_\K\d+(?=\.tex)' | sort -n | tail -1)
+
+        # If no files were found, set the number to 0, else increment the highest found number
+        if [ -z "$highest_num" ]; then
+          next_num=1
+        else
+          next_num=$((highest_num + 1))
+        fi
+
+        # Create a new file with the next number
+        new_file=$(printf "lec_%02d.tex" "$next_num")
+        full_path=$(realpath "$new_file")
+        touch "$new_file"
+        echo "Created new file at: $full_path"
+
+        # Add \lecture{} command at the top of the new file
+        current_date=$(date +"%d-%m-%Y")
+        lecture_command="\\lecture{$next_num}{$current_date}{title}{professor}"
+
+        # Insert the \lecture command at the beginning of the new file
+        echo -e "$lecture_command\n" | cat - "$new_file" > temp && mv temp "$new_file"
+
+
+
+        # Add the new file to master.tex before \end{document}
+        master_file="master.tex"
+        if [ -f "$master_file" ]; then
+          # Safely add the input line manually
+          temp_file=$(mktemp)
+          # Add \input before \end{document}
+          sed -i "/\\end{document}/i \\\\\input\{$new_file\}" "$master_file"
+          echo "Added \\input{$new_file} to $master_file"
+        else
+          echo "master.tex not found!"
+        fi
+      else
+      echo "Not implemented"
+      fi
+      '';
+  in [
+    specialisation
+  ];
+}
+
