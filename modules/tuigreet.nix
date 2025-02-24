a441942123ac03f3f64197ccdd0cbfb453b2d150
installer <installer@gmail.com>
2025-02-24 15:12:18 +0100

    sysupdate

--- modules/tuigreet.nix
+++ modules/tuigreet.nix
@@ -10,7 +10,7 @@
                  --asterisks \
                  --user-menu \
                  --remember \
-                 --theme 'text=white;time=yellow;container=black;border=blue;title=yellow;greet=green;prompt=bright_red;input=magenta;action=cyan;button=bright_yellow'
+                 --theme 'text=white;time=yellow;container=grey;border=yellow;title=white;greet=white;prompt=white;input=white;action=white;button=white'
         '';
       };
     };
