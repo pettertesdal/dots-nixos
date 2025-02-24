b87bd10244b3b4e43cf09adab6579ef3dec32827
installer <installer@gmail.com>
2025-02-24 17:40:41 +0100

    sysupdate

--- modules/tuigreet.nix
+++ modules/tuigreet.nix
@@ -1,13 +1,10 @@
 { config, lib, pkgs, ... }:
-let
-  command = "swww init && sleep 1 && Hyprland";
-in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd ${command}";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
       };
     };
   };
