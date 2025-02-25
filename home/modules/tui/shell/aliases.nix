5f5c5c4d97fead062e4ee82ea2c4130928d5ac14
installer <installer@gmail.com>
2025-02-25 16:40:49 +0100

    sysupdate

--- home/modules/tui/shell/aliases.nix
+++ home/modules/tui/shell/aliases.nix
@@ -2,5 +2,6 @@
   home.shellAliases = {
     systest = "sudo nixos-rebuild test --flake ~/.dots/";
     current = "cd ~/documents/university/current_course/";
+    zms = "zellij attach $(zellij ls -s | fzf)";
   };
 }
