051e08457b383a15ce1a59d3a606e48deed702cd
installer <installer@gmail.com>
2025-05-05 14:12:30 +0200

    sysupdate

--- configuration.nix
+++ configuration.nix
@@ -148,14 +148,6 @@
     #jack.enable = true;
   };
 
-  services.pipewire.wireplumber.extraConfig."10-bluez" = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = false;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = [];  # Remove HSP/HFP roles
-    };
-  };
 
 
   # Automatic garbage collect
